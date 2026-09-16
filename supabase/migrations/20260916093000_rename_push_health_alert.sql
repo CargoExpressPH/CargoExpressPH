102c58a5093e1b9666757c407ab9af1d5c73a4db
Kwen26 <kwinamigo@gmail.com>
2026-09-16 13:58:02 +0800

    fix: Remove BOM from rename_push_health_alert migration

--- supabase/migrations/20260916093000_rename_push_health_alert.sql
+++ supabase/migrations/20260916093000_rename_push_health_alert.sql
@@ -1,4 +1,4 @@
-﻿CREATE OR REPLACE FUNCTION private.monitor_push_delivery_health()
+CREATE OR REPLACE FUNCTION private.monitor_push_delivery_health()
 RETURNS VOID
 LANGUAGE plpgsql
 SECURITY DEFINER
